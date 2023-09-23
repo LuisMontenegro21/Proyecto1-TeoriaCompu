681643ad6f474996aeccbaa6fa1b9730a4f8da00
Gohan1233 <gar21352@uvg.edu.gt>
2023-09-22 20:36:21 -0600

    Luispe

--- AFD.py
+++ AFD.py
@@ -1,5 +1,5 @@
 # Archivo para la conversión del AFN a un AFD mediante construcción de subconjuntos
-#from graphviz import Digraph
+from graphviz import Digraph
 
 class AFD:
     def __init__(self, num_states, states, num_alphabet, alphabet, start, num_final, final_states, num_transitions, transitions):
